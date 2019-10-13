8e849934254efe8fb48c27030462360fafba1869
Jackey <smjjackey@sina.com>
2019-10-13 16:35:38 +0100

    Add one checking prime method, try dividing the prime in sqrt(num) Signed-off-by: Jackey <smjjackey@sina.com>

--- prime_number.py
+++ prime_number.py
@@ -2,7 +2,7 @@ import math
 def generate_prime():
     n=100
     pri_num_list=[]
-    for num in range(2, n + 1):
+    for num in range(2, n + 1): # the range of candidate numbers
         if all(num % j != 0 for j in range(2, int(math.sqrt(num))+1)):
             pri_num_list.append(num)
     print(len(pri_num_list),pri_num_list)
@@ -10,9 +10,9 @@ def generate_prime():
 def generate_prime2():
     n = 100
     pri_num_list = []
-    for num in range(2,n+1):
+    for num in range(2,n+1): # the range of candidate numbers
         flag = True
-        for j in range(2, int(math.sqrt(num)+1)):
+        for j in range(2, int(math.sqrt(num)+1)):  # Try divide 2 to sqrt(num)
             if(num%j==0):
                 flag=False
                 break
@@ -24,14 +24,23 @@ def generate_prime3():
     n = 100
     pri_num_list = []
     for num in range(2, n + 1):
-        flag = True
-        for j in range(2, int(math.sqrt(num) + 1)):
+        flag1 = True
+        for j in range(2, int(math.sqrt(num) + 1)): # Try divide 2 to sqrt(num)
+            flag2=True
+            ### check whether j is a prime ###
             for k in range(2, int(math.sqrt(j) + 1)):
                 if (j % k == 0):
-                    flag = False
+                    flag2 = False # j is not a prime
                 break
-        if (flag):
+            if(flag2==False):
+                continue
+            ### check whether j is a prime ###
+            if(num % j==0): # j is a prime
+                flag1=False # num is not a prime
+                break
+        if (flag1):
             pri_num_list.append(num)
     print(len(pri_num_list), pri_num_list)
 
 generate_prime2()
+generate_prime3()
