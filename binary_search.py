609591110a8246791714313243e740d23bfbe72d
Jackey <smjjackey@sina.com>
2020-08-09 20:13:22 +0800

    1. Add non-recursion method for binary_search
    Signed-off-by: Jackey <smjjackey@sina.com>

--- binary_search.py
+++ binary_search.py
@@ -7,26 +7,51 @@ def binarySearch(L,e,low,high):
     :param high: The highest index
     :return: bool: whether to find the number
     """
+    if(L==[]): return False
+
     if high == low:
         return L[low] == e
-    mid = (low+high)//2
+    elif low>high:
+        return False
+
+    mid = int((low+high)/2)
     if L[mid]==e:
         return True
     elif L[mid]>e:
-        if low == mid:
-            return False
-        else:
-            return binarySearch(L,e,low, mid-1)
+        # if low == mid: # equals to above statement "elif low>high"
+        #     return False
+        return binarySearch(L,e,low, mid-1)
     else:
-        if high == mid:
-            return False
+        # if high == mid: # equals to above statement "elif low>high"
+        #     return False
         return binarySearch(L,e,mid+1,high)
+
+def binarySearch2(L,e,low,high):
+    """
+    Binary Search
+    :param L: The sorted list
+    :param e: The number to look for
+    :param low: The lowest index
+    :param high: The highest index
+    :return: bool: whether to find the number
+    """
+    if(L==[]): return False
+    while(low<=high):
+        mid=(low+high)//2
+        if(L[mid]==e): return mid
+        elif(e<L[mid]):  high=mid-1
+        else: low=mid+1
+
+    return False
 
 def search(L,e):
     result = binarySearch(L,e,0,len(L)-1)
     print(result)
+    result2 = binarySearch2(L, e, 0, len(L) - 1)
+    print(result2)
 
-L = range(10);
-e = 8
 
-search(L,e)
+if __name__=="__main__":
+    L = range(10);
+    e = 8
+    search(L, e)
